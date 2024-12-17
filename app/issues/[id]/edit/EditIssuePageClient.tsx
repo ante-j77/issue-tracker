5ddc960e96daf44dde792b02f34ce8dd915a1f5f
Ante Juric <antetg77@gmail.com>
2024-12-18 00:42:54 +0100

    Improve the loading experience

--- app/issues/[id]/edit/EditIssuePageClient.tsx
+++ app/issues/[id]/edit/EditIssuePageClient.tsx
@@ -2,14 +2,15 @@
 
 import dynamic from "next/dynamic";
 import IssueFormSkeleton from "./loading";
+import { Issue } from "@prisma/client";
 
 const IssueForm = dynamic(() => import("@/app/issues/_components/IssueForm"), {
   ssr: false,
   loading: () => <IssueFormSkeleton />,
 });
 
-const NewIssuePage = () => {
-  return <IssueForm />;
+const EditIssuePageClient = ({ issue }: { issue: Issue }) => {
+  return <IssueForm issue={issue} />;
 };
 
-export default NewIssuePage;
+export default EditIssuePageClient;
--- app/issues/[id]/edit/page.tsx
+++ app/issues/[id]/edit/page.tsx
@@ -1,7 +1,8 @@
-import React from "react";
-import IssueForm from "../../_components/IssueForm";
 import prisma from "@/prisma/client";
 import { notFound } from "next/navigation";
+import dynamic from "next/dynamic";
+import IssueFormSkeleton from "./loading";
+import EditIssuePageClient from "./EditIssuePageClient";
 
 interface Props {
   params: Promise<{ id: string }>;
@@ -14,7 +15,7 @@ const EditIssuePage = async ({ params }: Props) => {
 
   if (!issue) notFound();
 
-  return <IssueForm issue={issue} />;
+  return <EditIssuePageClient issue={issue} />;
 };
 
 export default EditIssuePage;
